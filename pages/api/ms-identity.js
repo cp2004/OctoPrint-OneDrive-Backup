5f49b824d4d83bc5d10f76d86a94ef6e941dcfb6
Charlie Powell <31997505+cp2004@users.noreply.github.com>
2022-02-05 19:21:39 +0000

    Update ms-identity.js

--- pages/api/ms-identity.js
+++ pages/api/ms-identity.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
     res.status(200).json(
         {
             associatedApplications: [{
-                applicationId: "1fbab959-f7f1-43c4-a800-5f7f58eb068f"
+                applicationId: "85591a87-4e78-4ced-a394-3d61bcca04ab"
             }]
         }
     )
